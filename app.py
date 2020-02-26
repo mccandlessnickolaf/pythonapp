66295ca9751248ef7efd22907a89658ca9938fd5
mccandlessnickolaf <56041065+mccandlessnickolaf@users.noreply.github.com>
2020-02-26 13:03:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,5 @@ app = Flask(__name__)
 def hello():
     return 'Hello World!'
 
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+def get(self):
+     self.redirect("https://zippyloan.com/", True)
